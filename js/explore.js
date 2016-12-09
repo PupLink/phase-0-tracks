4f39d6ee72716f7c28ba844a87ef7d041cff080f
PupLink <dragster44d@gmail.com>
2016-12-09 00:55:02 -0600

    7.1 turn in

---
+++ js/explore.js
@@ -0,0 +1,27 @@
+// declair some variables
+// for (var counter = 0; counter < 11; counter++) {
+// 	console.log(counter);
+// }
+
+// for (var counter = 10; counter > -1; counter--) {
+// 	console.log(counter);
+// }
+
+//==================================================================
+
+//*** 7.1 ***
+
+// 1.) First i would put a sting in mind
+// 2.) I would initialize the counter, set its "while" condition to print while string length is not met
+// 3.) Then instead of using a '++' to print my string i would use a '--'
+// 4.) print to console
+
+var str = "Hello world!";
+
+for (var i = str.length; i >= 0; i--) {
+console.log(str[i]);
+}
+
+// as long as variable 1 is equal to variable 2, 
+// it will print true in colsole
+console.log( 1 == 1)
