9f33b8a3a4e29320a2a69079206c218b3bd62bbe
PupLink <dragster44d@gmail.com>
2017-01-04 17:04:06 -0600

    more sudo code to better understand my approach to 6.6 solo challenge

--- ruby/6_6Solo.rb
+++ ruby/6_6Solo.rb
@@ -4,7 +4,10 @@ total_chances = 6
 wrong_try = 0
 right_guess = ''
 
-#got creative for end results
+# got creative for end results had a lot of fun with art in the terminal... 
+# also found a different way to print results, gave it a shot and it seemed to work well
+
+#the '<<' method converts a given object into a string, found it useful to make little pictures in the console
 
 hanged = <<HANG
  +---+-
@@ -21,23 +24,23 @@ survived = <<WIN
 ____|_____
 WIN
 
-#puts words.inspect
+# puts words.inspect
 
 word = words[rand(words.length) - 1]
-#word = words[1]
+# word = words[1]
 
-#create a method to pull letter guesses and and make them '_' symbols
-def get_placeholder sample_word, guessed_words
-  placeholder = ''
+# create a method to pull letter guesses and and make them '_' symbols
+def get_space sample_word, guessed_words
+  space = ''
   sample_word.chars { |char| 
-    placeholder += (guessed_words.include? char)? char : '_'
+    space += (guessed_words.include? char)? char : '_'
   }
 
-  placeholder
+  space
 end
 
 puts "clear"
-puts "Guess what is:"+ get_placeholder(word, "")
+puts "Guess what is:"+ get_space(word, "")
 
 #loop required to guess over and over
 while true
@@ -50,15 +53,15 @@ while true
 
     if(right_guess.include? char)
       puts char + " is already given and accepted."
-      puts "Try another: " + get_placeholder(word, right_guess)
+      puts "Try another: " + get_space(word, right_guess)
     else
       right_guess = right_guess + char
-      placeholder = get_placeholder(word, right_guess)
+      space = get_space(word, right_guess)
 
-      puts "Great! " + placeholder
+      puts "Great! " + space
     end
 
-    unless placeholder.include? '_'
+    unless space.include? '_'
       puts "WELL DONE!! YOU SURVIVED"
       puts survived
       break
@@ -71,10 +74,10 @@ while true
     if (wrong_try == total_chances)
       puts "YOU HANGED!"
       puts hanged
-      break #i used this in claculator challenge, it seems like a usful actionable code line
+      break # i used this in claculator challenge, it seems like a usful actionable code line
     # catch all loop
     else
-      puts "Try another: " + get_placeholder(word, right_guess)
+      puts "Try another: " + get_space(word, right_guess)
     end
   end
 
