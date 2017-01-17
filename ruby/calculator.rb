e232cf751c1d2a4ed5ce6a6534944eb7704573d3
DJ Steele <dragster44d@gmail.com>
2017-01-17 04:30:26 -0600

    turning in quiz re attempt

--- ruby/calculator.rb
+++ ruby/calculator.rb
@@ -1,32 +1,42 @@
-#Calculator method needs 3 paramaters
-
 puts "Calculator Quiz\nJust write your input like you would on papper, \nbe sure to use spaces, then press enter \nEnter 'done' to quit."
 
 result = 0
+
 def calc (num1, sym, num2)
-  while true
+  until false
     print ">> " 
     str = gets.chomp.split(" ")  # splits into array, rejects blanks
-    return if str[0] == 'done'      # quit if first element is 'done'
+    calculations = []
+    calculations << str.join
 
-    num1 = str[0].to_i
-    num2 = str[2].to_i
-    sym = str[1].to_sym
+    if str[0] == "done"   then return  
+     all_calculations = calculations.length
+     p all_calculations.times {|i| puts calculations[i]}
+     true 
+  # quit if first element is 'done'
 
-    case sym
-    when :+ then puts num1 + num2
-    when :- then puts num1 - num2
-    when :* then puts num1 * num2
-    else
-      puts "invalid input"
-    end
-  end
-  
+end 
+
+num1 = str[0].to_i || str[0].to_str
+num2 = str[2].to_i
+sym = str[1].to_sym
+
+case sym
+when :+ then puts num1 + num2
+when :- then puts num1 - num2
+when :* then puts num1 * num2
+when :/ then puts num1 / num2
+else
+  puts "invalid input"
+end
+end  
+end
+
+
+until calc(0, '+', 0)
 
 end
 
-until calc(0, '+', 0) false
-	
-result += 1
-end
-p "#{result} calculations performed."
+# tried many ways to print the array 'calculations' but with no avail... 
+# really want to talk to my teacher about it, i really want to know this for the future...
+# if i still have one at DBC :(
