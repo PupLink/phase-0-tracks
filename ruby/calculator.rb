3312449418cc2ce39ddce0a839edd3cce01f665b
DJ Steele <dragster44d@gmail.com>
2017-01-20 08:40:34 -0600

    100% finished with week 5 solo

--- ruby/calculator.rb
+++ ruby/calculator.rb
@@ -1,41 +1,58 @@
 puts "Calculator Quiz\nJust write your input like you would on papper, \nbe sure to use spaces, then press enter \nEnter 'done' to quit."
-
-result = 0
+puts
+puts
 
 def calc (num1, sym, num2)
-  until false
-    print ">> " 
-    str = gets.chomp.split(" ")  # splits into array, rejects blanks
-    calculations = []
-    calculations << str.join
-
-    if str[0] == "done"   then return  
-     all_calculations = calculations.length
-     p all_calculations.times {|i| puts calculations[i]}
-     true 
-  # quit if first element is 'done'
-
-end 
-
-num1 = str[0].to_i || str[0].to_str
-num2 = str[2].to_i
-sym = str[1].to_sym
-
-case sym
-when :+ then puts num1 + num2
-when :- then puts num1 - num2
-when :* then puts num1 * num2
-when :/ then puts num1 / num2
-else
-  puts "invalid input"
-end
-end  
+  if sym == "+" 
+    num1 + num2
+  elsif sym == "-" 
+    num1 - num2
+  elsif sym == "*" 
+    num1 * num2
+  elsif sym == "/" 
+    num1 / num2
+  end 
 end
 
+str = gets.chomp
+calculations = []
 
-until calc(0, '+', 0)
+until str == "done"
+    
+  formatted_str = str.split(" ")
+  num1 = formatted_str[0].to_i
+  num2 = formatted_str[2].to_i
+  sym = formatted_str[1]
 
+  calc_return = calc(num1, sym, num2)
+  puts calc_return
+
+  # str = "3 + 3"
+  # calc_return = 6
+
+  # "3 + 3" + " = " + "6"
+  calculations << str + " = " + calc_return.to_s
+    # calculations << str.join
+
+    # if str[0] == "done"
+    #  all_calculations = calculations.length
+    #  all_calculations.times {|i| puts calculations[i]}
+    #  return true 
+    # end
+  str = gets.chomp
 end
+
+puts "calculations performed in this session..."
+puts
+
+# how to print a hash
+calculations.each {|num1, sym, num2| puts ">> #{num1} #{sym} #{num2}"}
+
+
+
+# until calc(0, '+', 0)
+
+# end
 
 # tried many ways to print the array 'calculations' but with no avail... 
 # really want to talk to my teacher about it, i really want to know this for the future...
